0ecacfae1a1799d67f0d3f9cbac256969733ef66
Ben Stoller <stollersystemsound@gmail.com>
2020-09-09 16:34:24 -0700

    add css styling
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- src/main.js
+++ src/main.js
@@ -8,14 +8,17 @@ import MartianWeather from './js/martian_weather.js';
 
 
 $(document).ready(function(){
+  
   let promise = MartianWeather.getWeather();
   promise.then(function(response) {
     const body = JSON.parse(response);
     const sol = body.sol_keys[0];
-    $(".season").text(`The current season is ${body[sol].Season}`);
-    $(".temperature").text(`The average temperature is ${body[sol].AT.av}`);
-    $(".windSpeed").text(`The current average windspeed is ${body[sol].HWS.av}`);
-    $(".pressure").text(`The current average pressure is ${body[sol].PRE.av}`);    
+    $(".season").html(`The current season is <u>${body[sol].Season}</u>`);
+    $(".temperature").html(`The average temperature is <b>${body[sol].AT.av} °F</b>`);
+    $(".maxTemperature").html(`The maximum temperature is <b>${body[sol].AT.mx} °F</b>`);
+    $(".minTemperature").html(`The minimum temperature is <b>${body[sol].AT.mn} °F</b>`);
+    $(".windSpeed").html(`The current average windspeed is <b>${body[sol].HWS.av} m/s</b>`);
+    $(".pressure").html(`The current average pressure is <b>${body[sol].PRE.av} Pa</b>`);    
   },function(error) {
     $(".showErrors").text(`There was an error processing your request: ${error}`);
   });
@@ -29,18 +32,27 @@ $(document).ready(function(){
     let promise = MartianWeather.getImage(rover,camera,date);
     promise.then(function(response){
       const body = JSON.parse(response);
-      console.log(body.photos)
+      // console.log(body.photos);
       if (body.photos.length === 0){
-        $(".showImages").text("Sorry no images for that rover/camera on that date!")    
+        $(".showImages").text("Sorry no images for that rover/camera on that date!");  
       } else {
-          body.photos.forEach(function(elem){
-            console.log(elem.img_src);
-            $(".showImages").append(`<img src=${elem.img_src}>`);
-          });
+        // $(".showImages").append(`
+        // <div class="carousel-item active">
+        //   <img class="d-block w-100" src='${body.photos[0].img_src}' alt="Slide 1">
+        // </div>`); 
+        // for (let i=1; i<body.photos.length; i++){
+        //   $(".showImages").append(`
+        //     <div class="carousel-item">
+        //       <img class="d-block w-100" src='${body.photo[i].img_src}' alt="Slide ${[i+1]}">
+        //     </div>`);
+        // }
+        body.photos.forEach(function(elem){          
+          $(".showImages").append(`<img src=${elem.img_src}>`);
+        });
       }
     },  function(error) {
-          $(".showErrors").text(`There was an error processing your request: ${error}`);
-        });
+        $(".showErrors").text(`There was an error processing your request: ${error}`);
+      });
   }); 
 });
 
