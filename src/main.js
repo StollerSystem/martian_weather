ff76dfbbcaf33606fe079ecf977b0765ef60b0f4
Ben Stoller <stollersystemsound@gmail.com>
2020-09-09 11:38:38 -0700

    add var for sol_keys
    Co-authored-by: Vanessa Stewart <vstewart1213@gmail.com>

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-import './js/martian_weather.js'
+import './js/martian_weather.js';
 import MartianWeather from './js/martian_weather.js';
 
 
@@ -11,13 +11,14 @@ $(document).ready(function(){
 
   let promise = MartianWeather.getWeather();
   promise.then(function(response) {
-    const body = JSON.parse(response); 
-    $(".season").text(`The current season is ${body["634"].Season}`)
-    $(".temperature").text(`The average temperature is ${body["634"].AT.av}`)
-    $(".windspeed")
-    $(".pressure")    
+    const body = JSON.parse(response);
+    const sol = body.sol_keys[0];
+    $(".season").text(`The current season is ${body[sol].Season}`);
+    $(".temperature").text(`The average temperature is ${body[sol].AT.av}`);
+    $(".windSpeed").text(`The current average windspeed is ${body[sol].HWS.av}`);
+    $(".pressure").text(`The current average pressure is ${body[sol].PRE.av}`);    
   }, function(error) {
     $(".showErrors").text(`There was an error processing your request: ${error}`);
   });
 
-})
+});
